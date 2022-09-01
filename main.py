a35f7da7345741e40c097edbf2085d9aaa5afe27
Jedddy <jedbalita25@gmail.com>
2022-09-01 14:21:21 +0800

    switched to class as there is too many globals

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ import os
 import threading
 
 
-is_paused = False
-is_playing = False
-VOLUME = 0.5
-
-
 """Create the Music Directory
 Initialize PyGame mixer"""
 try:
@@ -18,169 +13,132 @@ try:
 except FileExistsError:
     pass
 
-# Initialize Mixer
-pygame.mixer.init()
+pygame.mixer.init() # Initialize Mixer
 
+class YTMusicPLayer(ttk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.is_paused = False
+        self.is_playing = False
+        self.volume = 0.5
+        self.playing_label = ttk.Label(text="Insert Youtube link...", font="Times 10 italic bold")
+        self.playing_label.place(relx=0.5, rely=0.5, anchor="center")
+        self.link_entry = ttk.Entry(width=40)
+        self.link_entry.place(relx=0.5, rely=0.4, anchor="center")
+        self.play_btn = ttk.Button(text="Play", command=self.play_music)
+        self.play_btn.place(relx=0.5, rely=0.6, anchor="center")
+        self.pause_btn = ttk.Button(text="Pause", command=self.pause)
+        self.pause_btn.place(relx=0.3, rely=0.6, anchor="center")
+        self.stop_btn = ttk.Button(text="Stop", command=self.stop)
+        self.stop_btn.place(relx=0.7, rely=0.6, anchor="center")
+        self.volumeup_btn = ttk.Button(text="Volume +", command=self.volume_up)
+        self.volumeup_btn.place(relx=0.4, rely=0.75, anchor="center")
+        self.volumedown_btn = ttk.Button(text="Volume -", command=self.volume_down)
+        self.volumedown_btn.place(relx=0.6, rely=0.75, anchor="center")
+        self.informer = ttk.Label(text="Nothing is playing at the moment.",
+                                  font="Times 10 italic bold")
+        self.informer.place(relx=0.5, rely=0.3, anchor="center")
 
-"""Functions"""
+        # Create a new Thread
+        self.music_thread = threading.Thread(target=self.playing_thread)
+        self.music_thread.start()
 
+    def get_song_name(self):
+        """Gets song name from directory"""
+        song_name = os.listdir("./dl_music/")[0]
+        return song_name
 
-def get_song_name():
-    """Gets song name from directory"""
-    song_name = os.listdir("./dl_music/")[0]
-    return song_name
+    def pygame_player(self):
+        """Player Function"""
+        song_name = self.get_song_name()
 
+        if pygame.mixer.get_init() is None:
+            pygame.mixer.init()
+        pygame.mixer.music.load(f"./dl_music/{song_name}")
+        pygame.mixer.music.set_volume(self.volume)
+        pygame.mixer.music.play(-1)
 
-def pygame_player():
-    """Player Function"""
-    song_name = get_song_name()
+    def download(self):
+        """Downloads the Music"""
 
-    if pygame.mixer.get_init() is None:
-        pygame.mixer.init()
-    pygame.mixer.music.load(f"./dl_music/{song_name}")
+        """Starting name for the downloaded MP3 file,
 
-    pygame.mixer.music.set_volume(VOLUME)
+        that way os.listdir("./dl_music/")[0] will always return the last downloaded MP3."""
 
-    pygame.mixer.music.play(-1)
+        starting_name = 999
+        starting_name -= len(os.listdir("./dl_music/"))
 
+        # YTDL Options
+        ydl_opts = {
+            "format": "bestaudio/best",
+            "outtmpl": f"./dl_music/{starting_name}%(title)s.%(ext)s",
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "320",
+                }
+            ],
+        }
 
-def download():
-    """Downloads the Music"""
+        try:
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([self.link_entry.get()])
+        except Exception as e:
+            print("Download exception: " + str(e))
 
-    """Starting name for the downloaded MP3 file,
+    def play_music(self):
+        """Play Music"""
+        self.download()
+        song_name = self.get_song_name()
 
-    that way os.listdir("./dl_music/")[0] will always return the last downloaded MP3."""
-
-    starting_name = 999
-    starting_name -= len(os.listdir("./dl_music/"))
-
-    # YTDL Options
-    ydl_opts = {
-        "format": "bestaudio/best",
-        "outtmpl": f"./dl_music/{starting_name}%(title)s.%(ext)s",
-        "postprocessors": [
-            {
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": "mp3",
-                "preferredquality": "320",
-            }
-        ],
-    }
-
-    try:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([link_entry.get()])
-    except Exception as e:
-        print("Download exception: " + str(e))
-
-
-def play_music():
-    """Play Music"""
-    global is_playing
-
-    download()
-
-    song_name = get_song_name()
-
-    if not is_playing:
-        pygame_player()
-        is_playing = True
-    else:
-        pygame.mixer.music.stop()
-        pygame_player()
-        is_playing = True
-    informer.config(text=f"Playing: {song_name[3:-4]}")
-
-
-def pause():
-    global is_paused
-    global is_playing
-
-    if is_playing:
-        if is_paused:
-            pygame.mixer.music.unpause()
-            pause_btn.config(mnfrm, text="Pause")
-            is_paused = False
+        if not self.is_playing:
+            self.pygame_player()
+            self.is_playing = True
         else:
-            pygame.mixer.music.pause()
-            is_paused = True
-            pause_btn.config(mnfrm, text="Resume")
+            pygame.mixer.music.stop()
+            self.pygame_player()
+            self.is_playing = True
+        self.informer.config(text=f"Playing: {song_name[3:-4]}")
+
+    def pause(self):
+        if self.is_playing:
+            if self.is_paused:
+                pygame.mixer.music.unpause()
+                self.pause_btn.config(text="Pause")
+                self.is_paused = False
+            else:
+                pygame.mixer.music.pause()
+                self.is_paused = True
+                self.pause_btn.config(text="Resume")
+
+    def stop(self):
+        if self.is_playing:
+            pygame.mixer.music.stop()
+            self.is_playing = False
+            self.informer.config(text="Nothing is playing at the moment.")
+            # Quit the mixer so it doesn't cause issues when deleting the file.
+            pygame.mixer.quit()
+            # Removes all files inside the music directory
+            for i in os.listdir("./dl_music/"):
+                os.remove(f"./dl_music/{i}")
+
+    def volume_up(self):
+        self.volume += 0.1
+        pygame.mixer.music.set_volume(self.volume)
+
+    def volume_down(self):
+        self.volume
+        self.volume -= 0.1
+        pygame.mixer.music.set_volume(self.volume)
+
+    def playing_thread(self):
+        """Thread for the player"""
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
 
 
-def stop():
-    global is_playing
-
-    if is_playing:
-        pygame.mixer.music.stop()
-        is_playing = False
-
-        informer.config(mnfrm, text="Nothing is playing at the moment.")
-
-        # Quit the mixer so it doesn't cause issues when deleting the file.
-        pygame.mixer.quit()
-        # Removes all files inside the music directory
-        for i in os.listdir("./dl_music/"):
-            os.remove(f"./dl_music/{i}")
-
-
-def volume_up():
-    global VOLUME
-
-    VOLUME += 0.1
-    pygame.mixer.music.set_volume(VOLUME)
-
-
-def volume_down():
-    global VOLUME
-
-    VOLUME -= 0.1
-    pygame.mixer.music.set_volume(VOLUME)
-
-
-def playing_thread():
-    """Thread for the player"""
-    while pygame.mixer.music.get_busy():
-        pygame.time.Clock().tick(10)
-
-
-"""Initialize Window"""
-root = Tk()
-root.title("Youtube Loop Player")
-mnfrm = ttk.Frame(root, padding=10).grid()
-root.minsize(480, 270)
-root.maxsize(480, 270)
-
-
-playing_label = ttk.Label(
-    mnfrm, text="Insert Youtube link...", font="Times 10 italic bold"
-)
-playing_label.place(relx=0.5, rely=0.5, anchor="center")
-
-link_entry = ttk.Entry(mnfrm, width=40)
-link_entry.place(relx=0.5, rely=0.4, anchor="center")
-
-play_btn = ttk.Button(mnfrm, text="Play", command=play_music)
-play_btn.place(relx=0.5, rely=0.6, anchor="center")
-
-pause_btn = ttk.Button(mnfrm, text="Pause", command=pause)
-pause_btn.place(relx=0.3, rely=0.6, anchor="center")
-
-stop_btn = ttk.Button(mnfrm, text="Stop", command=stop)
-stop_btn.place(relx=0.7, rely=0.6, anchor="center")
-
-volumeup_btn = ttk.Button(mnfrm, text="Volume +", command=volume_up)
-volumeup_btn.place(relx=0.4, rely=0.75, anchor="center")
-
-volumedown_btn = ttk.Button(mnfrm, text="Volume -", command=volume_down)
-volumedown_btn.place(relx=0.6, rely=0.75, anchor="center")
-
-informer = ttk.Label(
-    mnfrm, text="Nothing is playing at the moment.", font="Times 10 italic bold"
-)
-informer.place(relx=0.5, rely=0.3, anchor="center")
-
-# Create a new Thread
-music_thread = threading.Thread(target=playing_thread)
-music_thread.start()
-
-root.mainloop()
+ytplayer = YTMusicPLayer()
+ytplayer.master.minsize(480, 270)
+ytplayer.master.maxsize(480, 270)
+ytplayer.mainloop()
